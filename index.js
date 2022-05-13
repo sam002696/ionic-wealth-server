e6996aa04a74e8f0605a8f3abbb507b909da628e
sam002696 <sadmansakib221@gmail.com>
2022-05-13 14:48:03 +0600

    seventh commit

--- index.js
+++ index.js
@@ -78,26 +78,34 @@ async function run() {
             res.json(cursor);
         });
 
+        // delete a document
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await documentsCollection.deleteOne(query);
+            res.json(result);
+        });
+
         // update user to admin role
-		app.put('/users/admin', async (req, res) => {
-			const user = req.body;
-			const filter = { email: user.email };
-			const updateDoc = { $set: { role: 'admin' } };
-			const result = await usersCollection.updateOne(filter, updateDoc);
-			res.json(result);
-		})
+        app.put('/users/admin', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const updateDoc = { $set: { role: 'admin' } };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.json(result);
+        })
 
         // check an user if admin
-		app.get('/users/:email', async (req, res) => {
-			const email = req.params.email;
-			const query = { email: email };
-			const user = await usersCollection.findOne(query);
-			let isAdmin = false;
-			if (user?.role === 'admin') {
-				isAdmin = true;
-			}
-			res.json({ admin: isAdmin });
-		});
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false;
+            if (user?.role === 'admin') {
+                isAdmin = true;
+            }
+            res.json({ admin: isAdmin });
+        });
     } catch {
         // await client.close();
     }
